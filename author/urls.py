a0ed4e683041dc9c62c56e8daa1401b4c9832618
michael awad <michaelawadeissa@gmail.com>
2024-08-26 16:30:00 +0300

    first commit

---
+++ author/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from .views import *
+urlpatterns=[
+    path('',author_list,name='author_list'),
+    path('Add/',author_create,name='author_create'),
+    path('Update/<int:id>',author_update,name='author_update'),
+    path('Delete/<int:id>',author_delete,name='author_delete'),
+    path('<int:id>',author_show,name='author_show')
+
+]
